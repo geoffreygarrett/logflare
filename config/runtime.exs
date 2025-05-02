71ae968b31804a40ada885470be35514a4f67467
geoffreygarrett <26066340+geoffreygarrett@users.noreply.github.com>
2025-05-02 18:29:18 +0200

    fix: configure Cainophile epgsql for IPv6
    Cainophile's epgsql connection for logical replication was failing with :nxdomain.
    Ecto was already correctly configured with IPv6 (),
    but Cainophile requires explicit configuration for IPv6 in epgsql.
    
    Added  to Cainophile's epgsql config to resolve DNS resolution errors.

--- config/runtime.exs
+++ config/runtime.exs
@@ -80,6 +80,20 @@ config :logflare,
 
 maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
 
+config :logflare, :cainophile_postgres,
+       epgsql: %{
+         port:
+           if(System.get_env("DB_PORT") != nil,
+               do: String.to_integer(System.get_env("DB_PORT")),
+           else: nil
+         ),
+         host: System.get_env("DB_HOSTNAME"),
+         username: System.get_env("DB_USERNAME"),
+         password: System.get_env("DB_PASSWORD"),
+         database: System.get_env("DB_DATABASE"),
+         socket_options: maybe_ipv6
+       }
+
 config :logflare,
        Logflare.Repo,
        filter_nil_kv_pairs.(
