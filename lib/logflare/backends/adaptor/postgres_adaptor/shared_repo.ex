72a0ad6de43f638a96111f9cfcceccfdb49b3107
geoffreygarrett <26066340+geoffreygarrett@users.noreply.github.com>
2025-05-05 23:24:48 +0200

    fix(shared-repo): forward socket_options to dynamic Postgres repos for IPv6 support

--- lib/logflare/backends/adaptor/postgres_adaptor/shared_repo.ex
+++ lib/logflare/backends/adaptor/postgres_adaptor/shared_repo.ex
@@ -23,7 +23,7 @@ defmodule Logflare.Backends.Adaptor.PostgresAdaptor.SharedRepo do
     config = backend.config
     default_pool_size = Application.fetch_env!(:logflare, :postgres_backend_adapter)[:pool_size]
     schema = Map.get(config, :schema)
-    socket_options = Application.fetch_env!(:logflare, :postgres_backend_adapter)[:socket_options]
+#    socket_options = System.get_env("POSTGRES_BACKEND_IPV6") in ~w(true 1), do: [:inet6], else: []
 
 
     url = Map.get(config, :url)
@@ -33,7 +33,8 @@ defmodule Logflare.Backends.Adaptor.PostgresAdaptor.SharedRepo do
       pool_size: Map.get(config, :pool_size, default_pool_size),
       # Wait until repo is fully up and running
       sync_connect: true,
-      socket_options: socket_options,
+#      socket_options: socket_options,
+      socket_options: [:inet6],
       after_connect: {__MODULE__, :__after_connect__, [schema]}
     ]
 
